e7a7308fa09ba486211abe6f696d37b4243c94a3
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-04-30 12:59:54 +0200

    Added resource names for generating pdfs.

--- src/main/java/services/PdfGenerator.java
+++ src/main/java/services/PdfGenerator.java
@@ -1,5 +1,7 @@
 package services;
 
+import static config.ResourceNames.PDFS_ROOT;
+import static config.ResourceNames.PDF_FILE_EXT;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -10,42 +12,39 @@ import com.itextpdf.layout.Document;
 
 public abstract class PdfGenerator<T> {
 
-	private final static String USER_HOME = System.getProperty("user.home");
-	private final static String PDFS_ROOT = USER_HOME + "/tpv/pdfs/";
-    private final static String PDF_FILE_EXT = ".pdf";
-    
-    protected Document pdfDocument;
-    
-    private void makeDirectories(String path) {
-        File file = new File(createPath(path));
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-        }
-    }
-    
-    private String createPath(String path) {
-    	if(!path.startsWith(PDFS_ROOT)) {
-    		path = PDFS_ROOT + path;
-    	}
-    	if(!path.endsWith(PDF_FILE_EXT)) {
-    		path += PDF_FILE_EXT;
-    	}
-    	return path;
-    }
-    
-    protected abstract String ownPath();
-    
-    protected abstract PageSize ownPageSize();
-    
-    private Document getPdfDocument(String path, PageSize pageSize) throws FileNotFoundException {
-        PdfWriter pdfWriter = new PdfWriter(createPath(path));
-        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
-        return new Document(pdfDocument, pageSize);
-    }
-    
+	protected Document pdfDocument;
+
+	private void makeDirectories(String path) {
+		File file = new File(createPath(path));
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+		}
+	}
+
+	private String createPath(String path) {
+		if (!path.startsWith(PDFS_ROOT)) {
+			path = PDFS_ROOT + path;
+		}
+		if (!path.endsWith(PDF_FILE_EXT)) {
+			path += PDF_FILE_EXT;
+		}
+		return path;
+	}
+
+	protected abstract String ownPath();
+
+	protected abstract PageSize ownPageSize();
+
+	private Document getPdfDocument(String path, PageSize pageSize) throws FileNotFoundException {
+		PdfWriter pdfWriter = new PdfWriter(createPath(path));
+		PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+		return new Document(pdfDocument, pageSize);
+	}
+
 	protected void generatePdf(T entity) throws FileNotFoundException {
 		String path = ownPath();
 		makeDirectories(path);
 		pdfDocument = getPdfDocument(path, ownPageSize());
 	}
+
 }
