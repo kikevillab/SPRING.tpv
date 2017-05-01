aebe41fa10e900fcee39226da0792485d32ab206
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-05-01 20:40:32 +0200

    Added custom exceptions.

--- src/test/java/api/PdfGenerationResourceFunctionalTesting.java
+++ src/test/java/api/PdfGenerationResourceFunctionalTesting.java
@@ -1,11 +1,15 @@
 package api;
 
+import static config.ResourceNames.DEFAULT_SEED_FILE;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.springframework.http.HttpStatus;
 
+import wrappers.FileNameWrapper;
 import wrappers.InvoiceIdWrapper;
 import wrappers.TicketIdWrapper;
 
@@ -18,19 +22,36 @@ public class PdfGenerationResourceFunctionalTesting {
     public void setUp(){
         new RestService().seedDatabase();
     }
-    
+
     @Test
-    public void testGenerateInvoicePdf() {
+    public void testGeneratePdfUnauthorized() {
+        thrown.expect(new HttpMatcher(HttpStatus.UNAUTHORIZED));
         new RestBuilder<Object>(RestService.URL).path(Uris.PDF_GENERATION + Uris.INVOICES)
         .body(new InvoiceIdWrapper(20170001))
+        .post()
+        .build();      
+        new RestBuilder<Object>(RestService.URL).path(Uris.PDF_GENERATION + Uris.TICKETS)
+        .body(new TicketIdWrapper(1))
+        .post()
+        .build();
+    }
+
+    @Test
+    public void testGenerateInvoicePdfWithManagerLoggedIn() {
+        String token = new RestService().registerAndLoginManager();
+        new RestBuilder<Object>(RestService.URL).path(Uris.PDF_GENERATION + Uris.INVOICES)
+        .body(new InvoiceIdWrapper(20170001))
+        .basicAuth(token, "")
         .post()
         .build();
     }
     
     @Test
-    public void testGenerateTicketPdf() {
+    public void testGenerateTicketPdfWithManagerLoggedIn() {
+        String token = new RestService().registerAndLoginManager();
         new RestBuilder<Object>(RestService.URL).path(Uris.PDF_GENERATION + Uris.TICKETS)
         .body(new TicketIdWrapper(1))
+        .basicAuth(token, "")
         .post()
         .build();
     }
