29eff094c9cf82e4bdecb36fbcd2906bbe1ce579
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-05-01 16:50:35 +0200

    Added wrapper for ticket id.

--- src/main/java/api/PdfGenerationResource.java
+++ src/main/java/api/PdfGenerationResource.java
@@ -1,5 +1,32 @@
 package api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import controllers.PdfGenerationController;
+import wrappers.TicketIdWrapper;
+
+@RestController
+@RequestMapping(Uris.VERSION + Uris.PDF_GENERATION)
 public class PdfGenerationResource {
 
+    private PdfGenerationController pdfGenController;
+    
+    @Autowired
+    public void setPdfGenerationController(PdfGenerationController pdfGenController){
+        this.pdfGenController = pdfGenController;
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void generateInvoicePdf(){
+        
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void generateTicketPdf(@RequestBody TicketIdWrapper ticketIdWrapper){
+        
+    }
 }
