63296717b73bf60eef05ba5b30fe4511c454c5ba
Sergio Banegas <sbanegas92@gmail.com>
2017-05-04 21:14:25 +0200

    e2e tests added

---
+++ src/main/webapp/ng-app/e2e/home/home.po.ts
@@ -0,0 +1,18 @@
+import { browser, element, by } from 'protractor';
+
+export class HomePage {
+
+	navigateTo() {
+		return browser.get('/home');
+	}
+
+	getPageTitleText() {
+		return element(by.id('pageTitle')).getText();
+	}
+
+	clickCartButton() {
+		return element(by.id('cartButton')).click(); 
+	}
+
+}
+
