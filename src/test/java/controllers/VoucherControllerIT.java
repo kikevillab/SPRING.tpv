552a5133aff48e9b761e04635067480660728059
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-05-11 18:28:56 +0200

    Added new tests

--- src/test/java/controllers/VoucherControllerIT.java
+++ src/test/java/controllers/VoucherControllerIT.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
@@ -16,6 +18,7 @@ import config.PersistenceConfig;
 import config.TestsControllerConfig;
 import config.TestsPersistenceConfig;
 import daos.core.VoucherDao;
+import wrappers.VoucherConsumptionWrapper;
 import wrappers.VoucherCreationWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,21 +27,46 @@ public class VoucherControllerIT {
 
     @Autowired
     private VoucherController voucherController;
-    
+
     @Autowired
     private VoucherDao voucherDao;
-         
+
     @Test
-    public void testCreateVoucher(){
+    public void testVoucherHasExpired() {
+        assertTrue(voucherController.voucherHasExpired(5));
+        assertFalse(voucherController.voucherHasExpired(4));
+    }
+
+    @Test
+    public void testCreateVoucher() {
         long previousCount = voucherDao.count();
         VoucherCreationWrapper voucherCreationWrapper = new VoucherCreationWrapper();
         voucherCreationWrapper.setValue(new BigDecimal(new Random().nextDouble()));
         Calendar dayAfter = Calendar.getInstance();
         dayAfter.add(Calendar.DAY_OF_MONTH, 1);
         voucherCreationWrapper.setExpiration(dayAfter);
-        voucherController.createVoucher(voucherCreationWrapper);    
+        voucherController.createVoucher(voucherCreationWrapper);
         assertEquals(previousCount + 1, voucherDao.count());
-        System.out.println(voucherDao.findAll());
-        voucherDao.deleteAll();
+        voucherDao.delete((int)voucherDao.count());
+    }
+
+    @Test
+    public void testConsumeVoucher() {
+        VoucherConsumptionWrapper voucherConsumptionWrapper = new VoucherConsumptionWrapper();
+        voucherConsumptionWrapper.setId(1);
+        voucherController.consumeVoucher(voucherConsumptionWrapper);
+        assertTrue(voucherDao.findOne(1).isConsumed());
+    }
+    
+    @Test
+    public void testIsVoucherConsumed(){
+        assertTrue(voucherController.isVoucherConsumed(4));
+        assertFalse(voucherController.isVoucherConsumed(3));
+    }
+    
+    @Test
+    public void testVoucherExists(){
+        assertTrue(voucherController.voucherExists(4));
+        assertFalse(voucherController.voucherExists(0));
     }
 }
