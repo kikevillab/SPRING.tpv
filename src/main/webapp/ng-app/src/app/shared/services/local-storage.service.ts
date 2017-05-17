2331a9dba57916e916339b45129f8bc4a461eb86
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:33:22 +0200

    #28 Fixes in "LocalStorageService" service from shared resources "/shared/services" to add a new method "isStoraged" to know if a there is something stored in local storage related with a key

---
+++ src/main/webapp/ng-app/src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class LocalStorageService {
+
+    setItem(key: string, value: any): void {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    getItem(key: string): any {
+        return localStorage.getItem(key);
+    }
+
+    removeItem(key: string): void {
+        localStorage.removeItem(key);
+    }
+
+    isStoraged(key: string): boolean {
+        return (this.getItem(key) !== null);
+    }
+}
