0dd4906f7af96564be47ddd688349b1cda7ee988
Fran López <fclopezjurado@gmail.com>
2017-05-17 22:42:48 +0200

    #28 Fixes in main application module "app.module" to add a dependency of the new "administration" module

--- src/main/webapp/ng-app/src/app/app.module.ts
+++ src/main/webapp/ng-app/src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { ToastyModule } from 'ng2-toasty';
-import { AppComponent } from './app.component';
-
-import { WelcomeModule } from './welcome/welcome.module';
-import { OrderTrackingModule } from './order-tracking/order-tracking.module';
-import { HomeModule } from './home/home.module';
-import { AppRoutingModule } from './app-routing.module';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ToastyModule} from 'ng2-toasty';
+import {AppComponent} from './app.component';
+import {WelcomeModule} from './welcome/welcome.module';
+import {OrderTrackingModule} from './order-tracking/order-tracking.module';
+import {HomeModule} from './home/home.module';
+import {AdminModule} from './admin/admin.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    WelcomeModule,
-    OrderTrackingModule,
-    HomeModule,
-    AppRoutingModule,
-    BrowserModule,
-    ToastyModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        WelcomeModule,
+        OrderTrackingModule,
+        HomeModule,
+        AppRoutingModule,
+        BrowserModule,
+        ToastyModule,
+        AdminModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
