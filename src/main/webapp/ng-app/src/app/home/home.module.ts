47c0b64799477d09a9bfc5fe080443d35c2d7f09
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:18:56 +0200

    #28 Fixes in definition of "home" module, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/home/home.module.ts
+++ src/main/webapp/ng-app/src/app/home/home.module.ts
@@ -1,53 +1,48 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from '@angular/material';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { ToastyModule } from 'ng2-toasty';
-
-
-import { HomeRoutingModule } from './home-routing.module';
-
-import { HomeComponent, OrderTrackingDialog } from './home.component';
-import { CartComponent } from './cart/cart.component';
-import { CalculatorComponent } from './cart/calculator/calculator.component';
-import { DateComponent } from '../shared/date.component';
-
-import { HTTPService } from '../shared/http.service';
-import { LocalStorageService } from '../shared/local-storage.service';
-import { ToastService } from '../shared/toast.service';
-
-import { CartModule } from './cart/cart.module';
-import { SearchModule } from './search/search.module';
-import { PaymentModule } from './payment/payment.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from '@angular/material';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {ToastyModule} from 'ng2-toasty';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent, OrderTrackingDialog} from './home.component';
+import {CartComponent} from './cart/cart.component';
+import {HTTPService} from '../shared/services/http.service';
+import {LocalStorageService} from '../shared/services/local-storage.service';
+import {ToastService} from '../shared/services/toast.service';
+import {CartModule} from './cart/cart.module';
+import {SearchModule} from './search/search.module';
+import {PaymentModule} from './payment/payment.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    FormsModule,
-    HttpModule,
-    JsonpModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    MaterialModule,
-    ToastyModule,
-    CartModule,
-    SearchModule,
-    PaymentModule,
-    NgxDatatableModule
-  ],
-  declarations: [
-    HomeComponent,
-    DateComponent,
-    CartComponent,
-    OrderTrackingDialog
-  ],
-  providers: [HTTPService, LocalStorageService, ToastService],
-  entryComponents: [OrderTrackingDialog]
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        FormsModule,
+        HttpModule,
+        JsonpModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MaterialModule,
+        ToastyModule,
+        CartModule,
+        SearchModule,
+        PaymentModule,
+        NgxDatatableModule,
+        SharedModule
+    ],
+    declarations: [
+        HomeComponent,
+        CartComponent,
+        OrderTrackingDialog
+    ],
+    providers: [HTTPService, LocalStorageService, ToastService],
+    entryComponents: [OrderTrackingDialog]
 })
-export class HomeModule { }
+export class HomeModule {
+}
