627952264c960a465c39f31111160d503b348534
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:23:41 +0200

    #28 Fixes in definition of "search" component, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/home/search/search.component.ts
+++ src/main/webapp/ng-app/src/app/home/search/search.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnDestroy, NgModule } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import { MdDialog } from '@angular/material';
+import {Component, OnDestroy, NgModule} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {MdDialog} from '@angular/material';
 
-import { HTTPService } from '../../shared/http.service';
-import { ToastService } from '../../shared/toast.service';
+import {HTTPService} from '../../shared/services/http.service';
+import {ToastService} from '../../shared/services/toast.service';
 
 @Component({
-  selector: 'search-view',
-  templateUrl: './search.component.html',
-  styles: [`
+    selector: 'search-view',
+    templateUrl: './search.component.html',
+    styles: [`
 
-  `]
+    `]
 })
 
 export class SearchComponent {
 
 
-  constructor (private toastService: ToastService, private httpService: HTTPService){
+    constructor(private toastService: ToastService, private httpService: HTTPService) {
 
-  }
+    }
 
 }
