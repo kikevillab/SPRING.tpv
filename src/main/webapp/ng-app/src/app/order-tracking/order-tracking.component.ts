566fd0963f12999d1173116cc4b4a1b72ffd3571
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:20:13 +0200

    #28 Fixes in definition of "order-tracking" component, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/order-tracking/order-tracking.component.ts
+++ src/main/webapp/ng-app/src/app/order-tracking/order-tracking.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import { Location } from '@angular/common';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Location} from '@angular/common';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
-import { ProductState } from './product-state';
-import { OrderTrackingService } from './order-tracking.service';
+import {ProductState} from './product-state';
+import {OrderTrackingService} from './order-tracking.service';
 
-import { TPVHTTPError } from '../shared/tpv-http-error';
+import {TPVHTTPError} from '../shared/models/tpv-http-error';
 
 @Component({
     selector: 'order-tracking-view',
@@ -14,47 +14,51 @@ import { TPVHTTPError } from '../shared/tpv-http-error';
         md-spinner {
             margin: 0 auto;
         }
+
         .green {
-            color:green;
+            color: green;
         }
+
         #backButton {
-            cursor:pointer;
+            cursor: pointer;
         }
+
         .red {
-            color:red;
+            color: red;
         }
 
         .red > button {
-            cursor:auto;
+            cursor: auto;
         }
     `]
 })
 export class OrderTrackingComponent {
 
-    ticketReference:string;
-    loading:boolean = true;
-    products:ProductState[];
-    error:boolean = false;
+    ticketReference: string;
+    loading: boolean = true;
+    products: ProductState[];
+    error: boolean = false;
     columns = [
-  { name: 'productCode' },
-  { name: 'description' },
-  { name: 'shoppingState' }
-  ];
+        {name: 'productCode'},
+        {name: 'description'},
+        {name: 'shoppingState'}
+    ];
 
-    constructor(private route:ActivatedRoute, private orderTrackingService:OrderTrackingService, private location: Location){}
-    
-    ngOnInit(){
-       this.ticketReference = this.route.snapshot.params['reference'];
-       this.orderTrackingService.getTicket(this.ticketReference).then((products:ProductState[]) => {
-           this.products = products;
-           this.loading = false;
-       }).catch((error:TPVHTTPError) => {
-           this.error = true;
-           this.loading = false;
-       }); 
+    constructor(private route: ActivatedRoute, private orderTrackingService: OrderTrackingService, private location: Location) {
     }
 
-    goToPreviousPage():void{
+    ngOnInit() {
+        this.ticketReference = this.route.snapshot.params['reference'];
+        this.orderTrackingService.getTicket(this.ticketReference).then((products: ProductState[]) => {
+            this.products = products;
+            this.loading = false;
+        }).catch((error: TPVHTTPError) => {
+            this.error = true;
+            this.loading = false;
+        });
+    }
+
+    goToPreviousPage(): void {
         this.location.back();
     }
 
