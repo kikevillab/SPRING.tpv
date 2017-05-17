d26dae4d48669ca3d60150726c61a09819d5c50b
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:20:33 +0200

    #28 Fixes in definition of "order-tracking" component, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/order-tracking/order-tracking.module.ts
+++ src/main/webapp/ng-app/src/app/order-tracking/order-tracking.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material';
+import {MaterialModule} from '@angular/material';
 
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 
 import {OrderTrackingComponent} from './order-tracking.component';
 import {OrderTrackingRoutingModule} from './order-tracking-routing.module';
 import {OrderTrackingService} from './order-tracking.service';
 
-import {HTTPService} from '../shared/http.service';
+import {HTTPService} from '../shared/services/http.service';
 
 @NgModule({
     declarations: [
