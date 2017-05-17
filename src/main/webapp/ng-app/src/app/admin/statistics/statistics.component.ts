076fdb2893f52cb107c499950c310928ea6c6b02
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:43:46 +0200

    #28 Definition of new "StatisticsComponent" component for "Statistics" section from "admin" module

---
+++ src/main/webapp/ng-app/src/app/admin/statistics/statistics.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by fran lopez on 13/05/2017.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    template: `<h1>Statistics</h1>`
+})
+export class StatisticsComponent {
+}
