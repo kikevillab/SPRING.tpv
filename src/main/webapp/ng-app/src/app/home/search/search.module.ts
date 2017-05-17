85e85567d7c25d83b118a606def9f1ae09a0f9ee
Fran López <fclopezjurado@gmail.com>
2017-05-17 23:24:05 +0200

    #28 Fixes in definition of "search" module, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/home/search/search.module.ts
+++ src/main/webapp/ng-app/src/app/home/search/search.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from '@angular/material';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { ToastyModule } from 'ng2-toasty';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from '@angular/material';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {ToastyModule} from 'ng2-toasty';
 
-import { SearchComponent } from './search.component';
+import {SearchComponent} from './search.component';
 
-import { HTTPService } from '../../shared/http.service';
-import { LocalStorageService } from '../../shared/local-storage.service';
-import { ToastService } from '../../shared/toast.service';
-import { ShoppingCartService } from '../shared/shopping-cart.service';
+import {HTTPService} from '../../shared/services/http.service';
+import {LocalStorageService} from '../../shared/services/local-storage.service';
+import {ToastService} from '../../shared/services/toast.service';
+import {ShoppingCartService} from '../shared/shopping-cart.service';
 
 @NgModule({
-  imports: [
-  CommonModule,
-  FormsModule,
-  HttpModule,
-  JsonpModule,
-  BrowserAnimationsModule,
-  FlexLayoutModule,
-  MaterialModule,
-  NgxDatatableModule,
-  ToastyModule
-  ],
-  declarations: [ SearchComponent ],
-  providers: [HTTPService, LocalStorageService, ToastService, ShoppingCartService]
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpModule,
+        JsonpModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MaterialModule,
+        NgxDatatableModule,
+        ToastyModule
+    ],
+    declarations: [SearchComponent],
+    providers: [HTTPService, LocalStorageService, ToastService, ShoppingCartService]
 })
-export class SearchModule { }
+export class SearchModule {
+}
