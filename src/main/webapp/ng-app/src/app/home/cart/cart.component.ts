4f04bc0af766294963b67411ddb902f5028a105b
Fran López <fclopezjurado@gmail.com>
2017-05-17 22:45:46 +0200

    #28 Fixes in Cart component, to change some dependencies paths. Paths of some shared resources have been changed after the last refactoring of "shared" application part

--- src/main/webapp/ng-app/src/app/home/cart/cart.component.ts
+++ src/main/webapp/ng-app/src/app/home/cart/cart.component.ts
@@ -1,115 +1,121 @@
-import { Component, OnDestroy, NgModule, Input, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import { MdDialog, MdSidenav } from '@angular/material';
+import {Component, OnDestroy, NgModule, Input, Output, EventEmitter} from '@angular/core';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {MdDialog, MdSidenav} from '@angular/material';
 
-import { CartProduct } from '../shared/cart-product';
+import {CartProduct} from '../shared/cart-product';
 
-import { ShoppingCartService } from '../shared/shopping-cart.service';
-import { ToastService } from '../../shared/toast.service';
+import {ShoppingCartService} from '../shared/shopping-cart.service';
+import {ToastService} from '../../shared/services/toast.service';
 
-import { TPVHTTPError } from '../../shared/tpv-http-error';
+import {TPVHTTPError} from '../../shared/models/tpv-http-error';
 
 @Component({
-  selector: 'cart-view',
-  templateUrl: './cart.component.html',
-  styles: [`
-    #paddingContainer {
-      padding:10em;
-      padding-top:0em;
-    }
-    #clearCartButton, #openCalculatorButton {
-      margin-left:0.4em;
-      float:right;
-    }
-    md-input-container {
-      width: 100%;
-    }
-    md-card {
-      margin-bottom:1em;
-    }
-    .center {
-      text-align:center;
-    }
-  `]
+    selector: 'cart-view',
+    templateUrl: './cart.component.html',
+    styles: [`
+        #paddingContainer {
+            padding: 10em;
+            padding-top: 0em;
+        }
+
+        #clearCartButton, #openCalculatorButton {
+            margin-left: 0.4em;
+            float: right;
+        }
+
+        md-input-container {
+            width: 100%;
+        }
+
+        md-card {
+            margin-bottom: 1em;
+        }
+
+        .center {
+            text-align: center;
+        }
+    `]
 })
 
 export class CartComponent implements OnDestroy {
 
-  @Output() closeSidenavEvent:EventEmitter<boolean>=new EventEmitter();
+    @Output() closeSidenavEvent: EventEmitter<boolean> = new EventEmitter();
 
-  codeInput:string = '';
-  totalPrice:string = this.shoppingCartService.getTotalPrice().toFixed(2);
-  cartProducts: CartProduct[] = this.shoppingCartService.getCartProducts();
-  subscription: Subscription;
-  columns = [
-  { name: 'description' },
-  { name: 'retailPrice' },
-  { name: 'amount' },
-  { name: 'discount' },
-  { name: 'delivered' },
-  { name: 'totalPrice'}
-  ];
+    codeInput: string = '';
+    totalPrice: string = this.shoppingCartService.getTotalPrice().toFixed(2);
+    cartProducts: CartProduct[] = this.shoppingCartService.getCartProducts();
+    subscription: Subscription;
+    columns = [
+        {name: 'description'},
+        {name: 'retailPrice'},
+        {name: 'amount'},
+        {name: 'discount'},
+        {name: 'delivered'},
+        {name: 'totalPrice'}
+    ];
 
-  constructor (private shoppingCartService: ShoppingCartService, private toastService: ToastService, private router: Router, public dialog: MdDialog){
-    this.subscription = this.shoppingCartService.getCartProductsObservable().subscribe((cartProducts:CartProduct[]) => {
-      this.cartProducts = cartProducts;
-      this.totalPrice = this.shoppingCartService.getTotalPrice().toFixed(2);
-    });
-  }
-
-  async onSubmit(cartForm:any, event:Event){ 
-  	event.preventDefault();
-  	this.shoppingCartService.addProduct(this.codeInput).then(()=>{
-     
-    }).catch((error:TPVHTTPError) =>{
-      this.toastService.error('Invalid code', error.description);
-    });
-    this.codeInput = '';
-  }
-
-  updateProduct(row:any, event:any, attribute:string):void {
-    let cartProduct:any = this.cartProducts[row.$$index];
-    if (attribute !== 'delivered'){
-      cartProduct[attribute] = Number(event.target.value);
-    } else {
-      cartProduct.delivered = !cartProduct.delivered;
+    constructor(private shoppingCartService: ShoppingCartService, private toastService: ToastService, private router: Router, public dialog: MdDialog) {
+        this.subscription = this.shoppingCartService.getCartProductsObservable().subscribe((cartProducts: CartProduct[]) => {
+            this.cartProducts = cartProducts;
+            this.totalPrice = this.shoppingCartService.getTotalPrice().toFixed(2);
+        });
     }
-    this.shoppingCartService.updateProduct(cartProduct);
-  }
 
-  updateTotalPrice(row:any, event:any):void {
-    let cartProduct:any = this.cartProducts[row.$$index];
-    let total:number = event.target.value;
-    cartProduct.discount = 100*(1-(total/(cartProduct.retailPrice*cartProduct.amount)));
-    this.shoppingCartService.updateProduct(cartProduct);
-  }
+    async onSubmit(cartForm: any, event: Event) {
+        event.preventDefault();
+        this.shoppingCartService.addProduct(this.codeInput).then(() => {
 
-  removeFromCart(cartProduct:CartProduct):void {
-    this.shoppingCartService.removeProduct(cartProduct);
-  }
+        }).catch((error: TPVHTTPError) => {
+            this.toastService.error('Invalid code', error.description);
+        });
+        this.codeInput = '';
+    }
 
-  clearCart():void {
-  	this.shoppingCartService.clear();
-  }
+    updateProduct(row: any, event: any, attribute: string): void {
+        let cartProduct: any = this.cartProducts[row.$$index];
+        if (attribute !== 'delivered') {
+            cartProduct[attribute] = Number(event.target.value);
+        } else {
+            cartProduct.delivered = !cartProduct.delivered;
+        }
+        this.shoppingCartService.updateProduct(cartProduct);
+    }
 
-  checkout():void {
-    this.router.navigate(['/home/payment']);
-    this.closeSidenavEvent.emit(true);
-  }
+    updateTotalPrice(row: any, event: any): void {
+        let cartProduct: any = this.cartProducts[row.$$index];
+        let total: number = event.target.value;
+        cartProduct.discount = 100 * (1 - (total / (cartProduct.retailPrice * cartProduct.amount)));
+        this.shoppingCartService.updateProduct(cartProduct);
+    }
 
-  ngOnDestroy(){
-  	this.subscription.unsubscribe();
-  }
+    removeFromCart(cartProduct: CartProduct): void {
+        this.shoppingCartService.removeProduct(cartProduct);
+    }
 
-  openCalculator(){
-    this.dialog.open(CalculatorDialog);
-  }
+    clearCart(): void {
+        this.shoppingCartService.clear();
+    }
+
+    checkout(): void {
+        this.router.navigate(['/home/payment']);
+        this.closeSidenavEvent.emit(true);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+    openCalculator() {
+        this.dialog.open(CalculatorDialog);
+    }
 
 }
 
 @Component({
-  selector: 'calculator-dialog',
-  template: `<calculator-view></calculator-view>`
+    selector: 'calculator-dialog',
+    template: `
+        <calculator-view></calculator-view>`
 })
-export class CalculatorDialog {}
+export class CalculatorDialog {
+}
