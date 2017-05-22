76cd22536829804d05cddfb0f0d97f9b3f4a56ce
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-05-22 18:37:16 +0200

    Remove from global testing.

--- src/test/java/api/AllFunctionalTesting.java
+++ src/test/java/api/AllFunctionalTesting.java
@@ -7,7 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({UserResourceFunctionalTesting.class, TokenResourceFunctionalTesting.class, DatabaseSeedResourceFunctionalTesting.class,
         ProductResourceFunctionalTesting.class, PdfGenerationResourceFunctionalTesting.class, TicketResourceFunctionalTesting.class,
-        /* VoucherResourceFunctionalTesting.class */
         InvoiceResourceFunctionalTesting.class, EmbroideryResourceFunctionalTesting.class, TextilePrintingResourceFunctionalTesting.class,
         ArticleResourceFunctionalTesting.class})
 
