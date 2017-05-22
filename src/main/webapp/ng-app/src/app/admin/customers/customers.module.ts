7cad2fc3bbe02c7826b32b66bab24a971ea46b77
Fran López <fclopezjurado@gmail.com>
2017-05-23 00:21:33 +0200

    #29 Adding definition of "Customers" sub-module from "admin" module.

--- src/main/webapp/ng-app/src/app/admin/customers/customers.module.ts
+++ src/main/webapp/ng-app/src/app/admin/customers/customers.module.ts
@@ -3,49 +3,17 @@
  */
 
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {UsersModule} from '../shared/users/users.module';
 import {CustomersComponent} from './customers.component';
-import {FilterComponent} from './filter/filter.component';
-import {ResultsComponent} from './results/results.component';
-import {NewCustomerDialog} from './new-customer/new-customer.component';
-import {HTTPService} from '../../shared/services/http.service';
-import {AngularMaterialModule} from '../../shared/angular-material.module';
-import {ToastService} from '../../shared/services/toast.service';
-import {CustomersService} from './customers.service';
-import {RegExpFormValidatorService} from '../../shared/services/reg-exp-form-validator.service';
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemoryDataService} from '../../shared/services/in-memory-data.service';
 
 @NgModule({
     declarations: [
-        CustomersComponent,
-        FilterComponent,
-        ResultsComponent,
-        NewCustomerDialog
+        CustomersComponent
     ],
     imports: [
         CommonModule,
-        FormsModule,
-        HttpModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        AngularMaterialModule,
-        InMemoryWebApiModule.forRoot(InMemoryDataService, {passThruUnknownUrl: true}),
-        NgxDatatableModule
-    ],
-    providers: [
-        HTTPService,
-        ToastService,
-        CustomersService,
-        RegExpFormValidatorService
-    ],
-    entryComponents: [
-        NewCustomerDialog
+        UsersModule
     ]
 })
 export class CustomersModule {
