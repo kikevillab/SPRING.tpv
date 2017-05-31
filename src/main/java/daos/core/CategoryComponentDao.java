57d51d353789dee0073a06a189f242c95a28b8dc
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-05-31 23:17:17 +0200

    Add Daos.

---
+++ src/main/java/daos/core/CategoryComponentDao.java
@@ -0,0 +1,7 @@
+package daos.core;
+import org.springframework.data.jpa.repository.JpaRepository;
+import entities.core.CategoryComponent;
+
+public interface CategoryComponentDao extends JpaRepository<CategoryComponent, Long>{
+
+}
