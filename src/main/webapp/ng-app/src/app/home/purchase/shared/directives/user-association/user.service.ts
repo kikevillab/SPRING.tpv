62d16d31fa69d51521140c44a8fd2c9c9d5a7e22
Sergio Banegas <sbanegas92@gmail.com>
2017-06-04 13:22:51 +0200

    Authorization headers now sent in every petition

--- src/main/webapp/ng-app/src/app/home/purchase/shared/directives/user-association/user.service.ts
+++ src/main/webapp/ng-app/src/app/home/purchase/shared/directives/user-association/user.service.ts
@@ -18,12 +18,9 @@ export class UserService {
   constructor (private httpService: HTTPService, private localStorageService: LocalStorageService) {}
 
   newUser(user: User): Promise<any> {
-    let sessionString: string = this.localStorageService.getItem(LOCAL_STORAGE_TOKEN_ATTRIBUTE);
-    let parsedSession = JSON.parse(sessionString);
-    let headers = new Headers();
-    headers.append('Authorization', 'Basic ' + btoa(parsedSession.token + ':'));
+    
     return new Promise((resolve: Function, reject: Function) => {
-      this.httpService.post(`${API_GENERIC_URI}${URI_USERS}?role=${ROLE_CUSTOMER}`, user, headers).subscribe(
+      this.httpService.post(`${API_GENERIC_URI}${URI_USERS}?role=${ROLE_CUSTOMER}`, user).subscribe(
         () => resolve(user),
         (error: TPVHTTPError) => {reject(error.description);}
       );
