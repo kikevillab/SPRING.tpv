12db8489eb9b1665f4692b93ae0ad2df94b44cde
Fran López <fclopezjurado@gmail.com>
2017-06-13 00:55:34 +0200

    #68 Fixes in "Users" model to add new constants

--- src/main/webapp/ng-app/src/app/shared/models/user.model.ts
+++ src/main/webapp/ng-app/src/app/shared/models/user.model.ts
@@ -5,6 +5,7 @@
 export const MOBILE_ATTRIBUTE_NAME: string = 'mobile';
 export const DNI_ATTRIBUTE_NAME: string = 'dni';
 export const EMAIL_ATTRIBUTE_NAME: string = 'email';
+export const ID_ATTRIBUTE_NAME: string = 'id';
 
 export class User {
     constructor(public mobile?: number, public password?: string, public dni?: string, public email?: string,
