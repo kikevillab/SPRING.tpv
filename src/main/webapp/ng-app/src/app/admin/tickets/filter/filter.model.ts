e2a7553f42d027c962a98629c2088c4a149dc3cf
Fran López <fclopezjurado@gmail.com>
2017-06-13 00:50:25 +0200

    #68 Fixes in "Filter" model for "FilterComponent" used in "Users" shared sub-module from "Admin" module, to change attributes names

--- src/main/webapp/ng-app/src/app/admin/tickets/filter/filter.model.ts
+++ src/main/webapp/ng-app/src/app/admin/tickets/filter/filter.model.ts
@@ -6,7 +6,7 @@ export const START_DATE_ATTRIBUTE_NAME: string = 'start_date';
 export const END_DATE_ATTRIBUTE_NAME: string = 'end_date';
 
 export class Filter {
-    constructor(public mobile?: number, public reference?: string, public start_date?: string,
+    constructor(public user?: number, public reference?: string, public start_date?: string,
                 public end_date?: string) {
     }
 }
