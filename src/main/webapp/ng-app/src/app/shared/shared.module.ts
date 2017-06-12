4027691bc3b11db7cff992ee79a4687e42cffe85
Fran López <fclopezjurado@gmail.com>
2017-06-13 00:52:25 +0200

    #68 Fixes in definition of "SharedModule" to add a new pipe to share in all application. This pipe is called "BooleanToStringPipe"

--- src/main/webapp/ng-app/src/app/shared/shared.module.ts
+++ src/main/webapp/ng-app/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 /**
-  * @author Fran López
-  * @author Sergio Banegas Cortijo
-  * Github: https://github.com/sergiobanegas 
-*/
+ * @author Fran López
+ * @author Sergio Banegas Cortijo
+ * Github: https://github.com/sergiobanegas
+ */
+
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DateComponent} from './directives/date.component';
 import {CapitalizePipe} from './pipes/capitalize.pipe';
+import {BooleanToStringPipe} from './pipes/bool-to-str.pipe';
+
 @NgModule({
     imports: [CommonModule],
-    declarations: [DateComponent, CapitalizePipe],
-    exports: [DateComponent, CapitalizePipe]
+    declarations: [DateComponent, CapitalizePipe, BooleanToStringPipe],
+    exports: [DateComponent, CapitalizePipe, BooleanToStringPipe]
 })
 export class SharedModule {
 }
