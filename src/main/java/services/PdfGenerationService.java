015c1343153b641a46f637bf8562a8a19542494e
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-06-13 19:53:39 +0200

    Fix getting resources.

--- src/main/java/services/PdfGenerationService.java
+++ src/main/java/services/PdfGenerationService.java
@@ -3,6 +3,8 @@ package services;
 import java.io.IOException;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import entities.core.Invoice;
@@ -11,7 +13,9 @@ import entities.core.Ticket;
 import entities.core.Voucher;
 
 @Service
+@Transactional
 public class PdfGenerationService {
+
     public byte[] generateInvoicePdf(Invoice invoice) throws IOException {
         return new InvoicePdfGenerator(invoice).generatePdf();
     }
@@ -23,7 +27,7 @@ public class PdfGenerationService {
     public byte[] generateVoucherPdf(Voucher voucher) throws IOException {
         return new VoucherPdfGenerator(voucher).generatePdf();
     }
-    
+
     public byte[] generateBarcodesPdf(List<Product> productList) throws IOException {
         return new BarcodesPdfGenerator(productList).generatePdf();
     }
