8aad6de437f56d96984122c5c8137b99508d5207
Fran López <fclopezjurado@gmail.com>
2017-06-15 22:49:49 +0200

    #69 Definition of "ShoppingState" model to manage the state associated to a shopping.

---
+++ src/main/angular/src/app/admin/shared/models/shopping-state.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by fran lopez on 14/06/2017.
+ */
+
+export const STATE_ATTRIBUTE_NAME: string = 'state';
+
+export class ShoppingState {
+    constructor(public id: number, public state: string) {
+    }
+}
