f8f466684c3909599009b47be02780e1b5e4be8f
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-06-21 01:41:07 +0200

    Re-active file deletion.

--- src/test/java/services/PdfGeneratorServiceIT.java
+++ src/test/java/services/PdfGeneratorServiceIT.java
@@ -113,7 +113,7 @@ public class PdfGeneratorServiceIT {
         assertTrue(pdfFile.canWrite());
     }
 
-    //@AfterClass
+    @AfterClass
     public static void tearDownOnce() throws IOException {
         String path = PDFS_ROOT + INVOICES_PDFS_ROOT + INVOICE_PDF_FILENAME_ROOT + 20170001 + PDF_FILE_EXT;
         new File(path).delete();
