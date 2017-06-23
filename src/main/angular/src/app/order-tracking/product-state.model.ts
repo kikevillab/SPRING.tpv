b397ad78f94bf0818bb93e0e33d39c579acf0a37
jbernal <jbernal@etsisi.upm.es>
2017-06-23 17:09:39 +0200

    delete angular

--- src/main/angular/src/app/order-tracking/product-state.model.ts
+++
@@ -1,7 +0,0 @@
-/**
-  * @author Sergio Banegas Cortijo
-  * Github: https://github.com/sergiobanegas 
-*/
-export class ProductState {
-    constructor(public description: string, public productCode: string, public shoppingState: string) { }
-}
--- src/main/angular/src/app/shared/models/tpv-http-error.model.ts
+++
@@ -1,7 +0,0 @@
-/**
-  * @author Sergio Banegas Cortijo
-  * Github: https://github.com/sergiobanegas 
-*/
-export class TPVHTTPError {
-    constructor(public error: string, public description: string, public url: string) { }
-}
